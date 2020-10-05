23f5c5426c839f9963dba0a80112cc2429844c42
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-10-04 19:44:02 -0700

    changes plot.js

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,7 +1,13 @@
-d3.json(".data/samples.json").then((importedData) => {
+d3.json("data/samples.json").then((importedData) => {
 
-console.log (importedData);
+console.log(importedData);
 
 var data = importedData;
 
-data.sort
+});
+
+// have metadata:Array of objects
+// have names:Array - they are the numbers of the individuals, which is their "id" in metadata & samples
+// samples:Array of objects 
+metadata = {id: 940, ethnicity: "Caucasian", gender: "F", age: 24, location: "Beaufort/NC", wfreq:2}
+//
