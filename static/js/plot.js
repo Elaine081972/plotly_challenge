c365219c381e6c6da030b8ce8ff5d27b4b40be51
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-10-07 14:15:58 -0700

    update plot.js

--- static/js/plot.js
+++ static/js/plot.js
@@ -6,14 +6,29 @@ var data = importedData;
 
 var samples = Object.values(data.samples);
 var metadata = Object.values(data.metadata);
+let names = Object.values(data.names);
 
 console.log(samples);
 console.log(metadata);
+console.log(names);
 
+// populate drop down with all 'names'/ID numbers (code adapted from YouTube/KodeBase/How to populate select options, Javascript)
+let select = document.querySelector('select');
+
+let options = names.map(names => `<option value=${names}>${names}</option>`).join('\n');
+select.innerHTML = options;
+
+// unpack function to get at the indexed data
+function unpack(rows, index) {
+    return rows.map(function(row) {
+        return row[index];
+    });
+}
+// 
 });
 
 // have metadata:Array of objects
 // have names:Array - they are the numbers of the individuals, which is their "id" in metadata & samples
 // samples:Array of objects 
-metadata = {id: 940, ethnicity: "Caucasian", gender: "F", age: 24, location: "Beaufort/NC", wfreq:2}
+//metadata = {id: 940, ethnicity: "Caucasian", gender: "F", age: 24, location: "Beaufort/NC", wfreq:2}
 //
